1f51e6523f050467ff4654907d5923947b40a38b
romeoliveirasantos <rjob.oliveira@gmail.com>
2024-07-20 03:26:03 -0300

    Audio on mouse hover - como deve ser.mp3

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const div1 = document.querySelector('.double-cards');
+const h5Header = document.querySelector('.hFrase');
+const audio = new Audio('assets/comoDeveSer.mp3');
 
+//play audio Como deve ser
+h5Header.addEventListener('mouseover', ()=>{
+    audio.currentTime = 0;
+    audio.play();   
+})
+
+//Card effects
 div1.addEventListener('click', ()=>{
   div1.classList.add('hidden')
   setTimeout(() => {
     div1.style.display = 'none'  
-  }, 1000);
-  
+  }, 1000);   
 })
