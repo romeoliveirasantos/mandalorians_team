c4ff7d1d9465aef350155cc11248e422302015b6
romeoliveirasantos <rjob.oliveira@gmail.com>
2024-07-20 03:29:43 -0300

    fix: icon page & audio

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const div1 = document.querySelector('.double-cards');
 const h5Header = document.querySelector('.hFrase');
-const audio = new Audio('assets/comoDeveSer.mp3');
+const audio = new Audio('./assets/comoDeveSer.mp3');
 
 //play audio Como deve ser
 h5Header.addEventListener('mouseover', ()=>{
